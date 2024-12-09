c3556b6cc24006d6414b2c55f49a8ba9dff98d64
Oliver Gray <115050339+Oliver259@users.noreply.github.com>
2024-12-09 16:54:25 +1000

    added lib

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import sys, os
+from pathlib import Path
+
+plugindir = Path.absolute(Path(__file__).parent)
+paths = (".", "lib", "plugin")
+sys.path = [str(plugindir / p) for p in paths] + sys.path
+
 import webbrowser
 import http.client
 import json
